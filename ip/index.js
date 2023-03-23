8634b7817b1c3b55b11af502fb51eaaf6c7b7741
tigerweirdo <temmuzcetiner@gmail.com>
2023-03-23 21:44:14 +0100

    bitti

--- ip/index.js
+++ ip/index.js
@@ -1,5 +1,5 @@
 //axios import buraya gelecek
-
+import axios from "axios";
 var benimIP;
 
 
@@ -70,3 +70,65 @@ async function ipAdresimiAl(){
 
 
 //kodlar buraya gelecek
+function cardInfo(info) {
+	const newCard = document.createElement("div");
+	newCard.classList.add("card");
+
+	const image = document.createElement("img");
+	image.setAttribute("src", info.ülkebayrağı)
+
+	const card = document.createElement("div");
+	card.classList.add("card-info");
+
+	const iPAdress = document.createElement("h3");
+	iPAdress.classList.add("ip");
+
+	const country = document.createElement("p");
+	country.classList.add("ulke");
+
+	const LatLong = document.createElement("p");
+
+	const city = document.createElement("p");
+
+	const time = document.createElement("p");
+
+	const money = document.createElement("p");
+
+	const isp = document.createElement("p");
+
+
+	newCard.append(image, card);
+
+	card.append(iPAdress);
+	card.append(country);
+	card.append(LatLong);
+	card.append(city);
+	card.append(time);
+	card.append(money);
+	card.append(isp);
+
+
+	iPAdress.textContent = info.sorgu;
+	country.textContent = `${info.ülke} (${info.ülkeKodu})`
+	LatLong.textContent = `Enlem: ${info.enlem}  Boylam: ${info.boylam}`;
+	city.textContent = `şehir: ${info.şehir}`;
+	time.textContent = `Saat dilimi: ${info.saatdilimi}`;
+	money.textContent = `Para birimi: ${info.parabirimi}`;
+	isp.textContent = `ISP: ${info.isp}`;
+
+	return newCard;
+}
+
+const newCard = document.querySelector(".cards");
+
+
+const connect = async function () {
+	await ipAdresimiAl();
+	axios
+		.get("https://apis.ergineer.com/ipgeoapi/" + benimIP)
+		.then((res) => {
+			newCard.append(cardInfo(res.data));
+		})
+};
+
+connect();
